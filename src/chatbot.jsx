1d74545c6d26350e12e44f6f4112de952965732e
Rizki Helmi <77191093+rizkihelmi10@users.noreply.github.com>
2024-09-23 05:59:17 +0800

    Update chatbot.jsx

--- src/chatbot.jsx
+++ src/chatbot.jsx
@@ -26,7 +26,7 @@ const ChatBot = () => {
   
     try {
       console.log('User input:', userInput); // Log user input
-      const response = await axios.post('https://backend-chat-umber.vercel.app/chat/', { user_input: userInput });
+      const response = await axios.post('https://chatbotbackend-three.vercel.app/chat/', { user_input: userInput });
       
       console.log('API Response:', response); // Log API response
   
